6ce2b400a9944ffec2997ed9f43028749fd0ecaa
joedarby <joedarby87@gmail.com>
2016-10-04 02:25:01 +0100

    Add jackson annotation to terminal data point

--- app/data/TerminalDataPoint.java
+++ app/data/TerminalDataPoint.java
@@ -1,11 +1,14 @@
 package data;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 /**
  * Created by Joe on 30/09/2016.
  */
 public class TerminalDataPoint implements Comparable<TerminalDataPoint> {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
     public Date timestamp;
     public  String flowRate;
 
