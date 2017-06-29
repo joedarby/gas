02b5c829bedd2d77e57e8a6766d3448fc6269d56
joedarby <joedarby87@gmail.com>
2017-06-29 01:09:36 +0100

    begin firebase functionality

---
+++ app/modules/FirebaseMessageModule.java
@@ -0,0 +1,15 @@
+package modules;
+
+import play.libs.ws.WSClient;
+
+import javax.inject.Inject;
+
+public class FirebaseMessageModule {
+
+    @Inject
+    public void sendFirebaseMessage(WSClient ws, String message) {
+
+
+    }
+
+}
